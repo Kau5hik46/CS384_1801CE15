8cc82ba92bb3938b24c43f375762f299c855c43c
Kaushik <kaushikkomanduri@gmail.com>
2020-09-23 23:06:19 +0530

    Completed mean function using summation function

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -8,7 +8,7 @@ def summation(first_list = []):
     for _ in first_list:
         mean_value += _
 
-    return round(summation_value, 3)
+    return summation_value
 
 def sorting(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
@@ -21,35 +21,37 @@ def sorting(first_list = []):
         first_list[i], first_list[min_index] = first_list[min_index], first_list[i]
 
     sorted_list = first_list
-    return [round(num, 3) for num in sorted_list]
+    return sorted_list
 
 
 # Function to compute mean
 def mean(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
 
-    return round(mean_value, 3)
+    mean_value = summation(first_list)/len(first_list)
+
+    return mean_value
 
 
 # Function to compute median. You cant use Python functions
 def median(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
-
-    return round(median_value, 3)
+    
+    return median_value
 
 
 # Function to compute Standard deviation. You cant use Python functions
 def standard_deviation(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
 
-    return round(standard_deviation_value, 3)
+    return standard_deviation_value
 
 
 # Function to compute variance. You cant use Python functions
 def variance(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
 
-    return round(variance_value, 3)
+    return variance_value
 
 
 # Function to compute RMSE. You cant use Python functions
@@ -57,7 +59,7 @@ def rmse(first_list = [], second_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
     if(not any(isinstance(x, (int, float)) for x in second_list)): return 0
 
-    return round(rmse_value, 3)
+    return rmse_value
 
 
 # Function to compute mse. You cant use Python functions
@@ -65,7 +67,7 @@ def mse(first_list = [], second_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
     if(not any(isinstance(x, (int, float)) for x in second_list)): return 0
 
-    return round(mse_value, 3)
+    return mse_value
 
 
 # Function to compute mae. You cant use Python functions
@@ -73,7 +75,7 @@ def mae(first_list = [], second_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
     if(not any(isinstance(x, (int, float)) for x in second_list)): return 0
 
-    return round(mae_value, 3)
+    return mae_value
 
 
 # Function to compute NSE. You cant use Python functions
@@ -81,7 +83,7 @@ def nse(first_list = [], second_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
     if(not any(isinstance(x, (int, float)) for x in second_list)): return 0
 
-    return round(nse_value, 3)
+    return nse_value
 
 
 # Function to compute Pearson correlation coefficient. You cant use Python functions
@@ -89,18 +91,18 @@ def pcc(first_list = [], second_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
     if(not any(isinstance(x, (int, float)) for x in second_list)): return 0
 
-    return round(pcc_value, 3)
+    return pcc_value
 
 
 # Function to compute Skewness. You cant use Python functions
 def skewness(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
 
-    return round(skewness_value, 3)
+    return skewness_value
     
 
 # Function to compute Kurtosis. You cant use Python functions
 def kurtosis(first_list = []):
     if(not any(isinstance(x, (int, float)) for x in first_list)): return 0
 
-    return round(kurtosis_value, 3)
+    return kurtosis_value
