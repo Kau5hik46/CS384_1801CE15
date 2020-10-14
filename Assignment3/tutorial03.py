c7afe8d44c06621b671761fee5323c83c32360e5
Kaushik <kaushikkomanduri@gmail.com>
2020-10-14 18:01:01 +0530

    Completed course() function and created necessary functions to simplify the code

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -14,9 +14,71 @@ with open(input_file_path, 'r') as input_file:
 	for row in reader:
 		raw_data.append(row)
 
+pwd = os.getcwd()
+
+def open_dir(directory = ".", pwd = os.getcwd()):
+	if(directory == "."):
+		return
+
+	try:
+		os.chdir(directory)
+	except:
+		os.mkdir(os.path.join(pwd, str(directory)))
+		os.chdir(directory)
+
+	print(os.getcwd())
+	return None
+
+def get_ydc(row):
+	student_id = row[headers[0]]
+	y,d,c = None, None, None
+	if(len(student_id) == 8):
+   		try:
+   			y = int(student_id[0:2])
+   			d = student_id[2:4]
+   			c = student_id[4:6]
+   		except:
+   			pass
+   		
+	return (y,d,c)
+
+def append_row(filename, list_of_elems):
+	if(os.path.exists(filename)):
+		with open(filename, 'a') as output_file:
+			writer = csv.writer(output_file, delimiter = ",")
+			writer.writerow(list_of_elems)
+	else:
+		with open(filename, 'w') as output_file:
+			writer = csv.writer(output_file, delimiter = ",")
+			writer.writerow(headers)
+			writer.writerow(list_of_elems)
+
+
 def course():
-	# Read csv and process
-	pass
+	open_dir("analytics")
+	open_dir("course")
+	course_dir = os.getcwd()
+
+	courses = set()
+	years = set()
+	degrees = {"01" : "btech", "11" : 'mtech', "12" : 'msc', "21" : 'phd'}
+
+	for row in raw_data:
+		year,degree,course = get_ydc(row)
+		years.add(year)
+		courses.add(course)
+
+	for row in raw_data:
+   		year,degree,course = get_ydc(row)
+   		if(year == None or degree == None or course == None):
+   			append_row("misc.csv", row.values())
+   			continue
+   		open_dir(course.lower(), os.getcwd())
+   		open_dir(degrees[degree].lower(), os.getcwd())
+   		filename = str(year).lower() + "_" + str(course).lower() + "_" + str(degrees[degree]).lower() + ".csv"
+   		append_row(filename, row.values())
+   		open_dir(course_dir)
+
 
 
 def country():
@@ -53,3 +115,6 @@ def blood_group():
 def new_file_sort():
     # Read csv and process
     pass
+
+course()
+
