3c208a88d29028fbcce20073ea9474889d54d6aa
Kaushik <kaushikkomanduri@gmail.com>
2020-10-14 16:33:35 +0530

    Imported modules and captured data from input file

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -1,9 +1,22 @@
 import csv
+import os
+from os import system as terminal
 
+# Imported necessary modules captured data from input_file
+input_file_path = 'studentinfo_cs384.csv'
+
+headers = []
+raw_data = []
+
+with open(input_file_path, 'r') as input_file:
+	reader = csv.DictReader(input_file)
+	headers = reader.fieldnames
+	for row in reader:
+		raw_data.append(row)
 
 def course():
-    # Read csv and process
-    pass
+	# Read csv and process
+	pass
 
 
 def country():
