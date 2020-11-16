59f49815f2865b7de34d9e367d623e9ca67acb07
Kaushik <kaushikkomanduri@gmail.com>
2020-11-16 10:03:46 +0530

    Completed rename_FIR and fixed few bugs in the previous commit

--- Assignment 5/tutorial05.py
+++ Assignment 5/tutorial05.py
@@ -2,45 +2,53 @@ import os
 import re
 
 pwd = os.getcwd #function alias
+
 # Opens folder if exists, else creates and opens
-def open_dir(directory = ".", pwd = pwd()):
+def open_dir(directory = ".", cwd = pwd()):
 	if directory == ".":
 		return
 
 	try:
 		os.chdir(directory)
 	except:
-		os.mkdir(os.path.join(pwd, str(directory)))
+		os.mkdir(os.path.join(cwd, str(directory)))
 		os.chdir(directory)
 
 	return pwd()
 
+root_folder = open_dir("Subtitles") #Setting this as the root folder for further reference
 
 def rename_FIR(folder_name = "FIR"):
-    # rename Logic 
-    
+	open_dir(folder_name)
+	Subtitles = os.listdir(pwd())
+	for subtitle in Subtitles:
+		try:
+			extracted = re.findall(r'\d+',subtitle)
+			episode_number = extracted[0].strip()
+			file_extension = (re.split(r'\.',subtitle)[-1]).strip()
+			episode_number = padding(episode_number, padding_episode)
+			new_name = "FIR - Episode " + episode_number + "." + file_extension
+			os.rename(subtitle, new_name)
+		except:
+			print(subtitle + " doesn't corespond to the normal subtitle naming convention")
+	return 
 
 def rename_Game_of_Thrones(folder_name = "Game of Thrones"):
-    # rename Logic 
-    
+	pass
+
 
 def rename_Sherlock(folder_name = "Sherlock"):
-    # rename Logic 
-    
+	pass
 
 def rename_Suits(folder_name = "Suits"):
-    # rename Logic 
-    
+	pass
 
 def rename_How_I_Met_Your_Mother(folder_name = "How I Met Your Mother"):
-    # rename Logic 
-    
+	pass
 
 # Driver Code
 
-root_folder = open_dir("Subtitles")
-
-series = ["FIR" : 1, "Game of Thrones" : 2, "How I Met Your Mother": 3, "Sherlock" : 4, "Suits" : 5]
+series = {"FIR" : 1, "Game of Thrones" : 2, "How I Met Your Mother": 3, "Sherlock" : 4, "Suits" : 5}
 
 def prompt():
 	series_input = str(input('''1.FIR
@@ -56,7 +64,7 @@ Please enter the title of the Series: '''))
 def rename(option = 0):
 	if(option == 0):
 		return
-	elif  option == series["FIR"]:
+	elif option == series["FIR"]:
 		rename_FIR()
 	elif option == series["Game of Thrones"]:
 		rename_Game_of_Thrones()
